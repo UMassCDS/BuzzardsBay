1a5ffe8dc0b69fc0626557edb448bdace76d31fd
bwcompton <bcompton@umass.edu>
2025-03-19 14:25:41 -0400

    Update README; a couple of minor things. Successful check - ready to merge

--- R/make_facet_plot.R
+++ R/make_facet_plot.R
@@ -7,6 +7,7 @@
 #' @return A ggplot2 object
 #' @import ggplot2
 #' @importFrom tidyr pivot_longer
+#' @importFrom rlang .data
 #' @keywords internal
 
 
@@ -14,15 +15,15 @@ make_facet_plot <- function(daily) {
 
 
    daily_long <- daily[, c('Date', 'DO_mean', 'Temp')] |>
-      tidyr::pivot_longer(cols = c(DO_mean, Temp))
+      tidyr::pivot_longer(cols = c(.data$DO_mean, .data$Temp))
 
    y_labels <- as_labeller(c(
       DO_mean = "Dissolved Oxygen (mg/L)",
       Temp = "Water Temperature (C)"
    ))
 
-   ggplot(daily_long, aes(x = Date, y = value)) +
-      geom_line(aes(color = name)) +
+   ggplot(daily_long, aes(x = .data$Date, y = .data$value)) +
+      geom_line(aes(color = .data$name)) +
       facet_grid(name ~ ., scales = 'free_y', switch = 'y', labeller = y_labels) +
       labs(y = 'Metric', x = 'Date') +
       theme_minimal() +
